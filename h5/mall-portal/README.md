180f85bfcb4a323e2ec8b2b0e31aea194233040f
bbbscxy <dreamx5008@163.com>
2020-08-25 15:58:36 +0800

    添加admin工程

--- h5/mall-portal/README.md
+++
@@ -1,6 +0,0 @@
-# react-H5
-## 安装步骤
-1、npm install 安装所有依赖  
-2、npm run server 启动服务  
-3、npm start 访问服务  
-4、localhost:4000/home.html 打开浏览器
--- h5/mall-portal/src/common/api/GoodsApi.js
+++
@@ -1,8 +0,0 @@
-import * as fetch from 'common/fetch.js';
-
-/**
- * 获取商品详情
- */
-export function goodsDetail({goodsId}) {
-  return fetch.get('/api/shop/goods/goodsDetail?goodsId='+goodsId);
-}
